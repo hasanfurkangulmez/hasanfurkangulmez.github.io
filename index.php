339204fa972affab264d1a409579d647ced7170f
Hasan Furkan Gülmez <108900212+hasanfurkangulmez@users.noreply.github.com>
2024-07-01 20:50:27 +0300

    Create index.html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
+      <meta name="description" content="Hasan Furkan Gülmez - Software Developer" />
+      <meta name="author" content="Hasan Furkan Gülmez"/>
+      <title>Hasan Furkan Gülmez - Yazılım Geliştirici</title>
+      <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    </head>
+    <body>
+      <h1>Hasan Furkan Gülmez</h1>
+      <h2>Yazılım Geliştirici</h2>
+    </body>
+</html>
